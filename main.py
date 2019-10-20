eca38fbb98c0b8653323ed390fa65ef73898f12d
js1804 <mra_arabzadeh@yahoo.com>
2019-10-20 22:29:28 +0330

    a* search completed and bfs changed to what sinadi sayedgit add main.py git add main.py

--- main.py
+++ main.py
@@ -75,20 +75,27 @@ def make_graph_from_file(path_of_file):
             
 def start_bfs(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods):
     queue = deque()
-    queue.append(p_location) 
-    bfs(queue, num_of_line, size_of_line, p_location, p_foods, q_foods, pq_foods)
-    queue.append(q_location)
-    bfs(queue, num_of_line, size_of_line, q_location, q_foods, p_foods, pq_foods)
+    while len(p_foods + q_foods + pq_foods) != 0:
+        print(p_foods, q_foods, pq_foods, p_location, q_location)
+        if p_location != 0 and len(p_foods + pq_foods) != 0:
+            # print('p')
+            queue.append(p_location) 
+            p_location = bfs(queue, num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods)
+        if q_location != 0 and len(q_foods + pq_foods) != 0:
+            # print('q')
+            queue.append(q_location)
+            q_location = bfs(queue, num_of_line, size_of_line, q_location, p_location, q_foods, p_foods, pq_foods)
 
-def bfs(queue, num_of_line, size_of_line, p_location, p_foods, q_foods, pq_foods):
-    pmatrix = [[False for x in range(size_of_line)]for y in range(num_of_line)]    
+def bfs(queue, num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods):
+    pmatrix = [[False for x in range(size_of_line)]for y in range(num_of_line)]   
+    pmatrix[int(p_location/size_of_line)][p_location%size_of_line]=True 
     while queue:
         curr = queue.popleft()
         
         # for x in pmatrix:
         #     l = [' 'if y==False else '*' for y in x]
         #     print(l)
-        # time.sleep(.07)
+        # time.sleep(.05)
         
         # tmp = sp.call('cls',shell=True)
         for nei in LINK_STATE[curr]:
@@ -97,9 +104,13 @@ def bfs(queue, num_of_line, size_of_line, p_location, p_foods, q_foods, pq_foods
                 pmatrix[int(nei/size_of_line)][nei%size_of_line]=True
             if nei in p_foods:
                 p_foods.remove(nei)
+                queue.clear()
+                return nei
             if nei in pq_foods:
                 pq_foods.remove(nei)
-    return 
+                queue.clear()
+                return nei
+    return p_location
 
 def start_ids(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods):
     max_depgh = (num_of_line-1) * (size_of_line-1)
@@ -144,6 +155,10 @@ def start_Astar(num_of_line, size_of_line, p_location, q_location, p_foods, q_fo
             break
 
         for item in LINK_STATE[curr.get_location()]:
+            if curr.get_agent() == 'p' and len(p_foods) + len(pq_foods) == 0:
+                break
+            if curr.get_agent() == 'q' and len(q_foods) + len(pq_foods) == 0:
+                break
             newitem = NodeState(item, curr.get_agent(), size_of_line)
             newitem.manhatan_distante(p_foods + pq_foods if curr.get_agent() == 'p' else q_foods + pq_foods)
             newitem.add_price(curr.get_price() + 1)
@@ -151,19 +166,39 @@ def start_Astar(num_of_line, size_of_line, p_location, q_location, p_foods, q_fo
             if curr.get_agent() == 'p':
                 if item in p_foods:
                     p_foods.remove(item)
+                    change_state(burder, LINK_STATE[curr.get_location()], LINK_STATE[q_location],size_of_line)
+                    break
                 elif item in pq_foods:
                     pq_foods.remove(item)
+                    change_state(burder, LINK_STATE[curr.get_location()], LINK_STATE[q_location],size_of_line)
+                    break
             else:
                 if item in q_foods:
                     q_foods.remove(item)
+                    change_state(burder, LINK_STATE[p_location], LINK_STATE[curr.get_location()],size_of_line)
+                    break
                 elif item in pq_foods:
                     pq_foods.remove(item)
+                    change_state(burder, LINK_STATE[p_location], LINK_STATE[curr.get_location()],size_of_line)
+                    break
     return 
+def change_state(heap, p_nei, q_nei, size_of_line):
+    heap = []
+    for item in p_nei:
+        newitem = NodeState(item, 'p', size_of_line)
+        newitem.add_price(1)
+        heap.append(newitem)
+    for item in q_nei:
+        newitem = NodeState(item, 'q', size_of_line)
+        newitem.add_price(1)
+        heap.append(newitem)
+    heapq.heapify(heap)
+    return heap
+
 for i in range(1,6):
     link_state, num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods= make_graph_from_file(PATH+str(i))
     LINK_STATE = link_state.copy()
-    print(i, num_of_line, size_of_line)
-    # start_bfs(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods,pq_foods)
+    start_bfs(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods,pq_foods)
     # start_ids(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods)
-    start_Astar(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods)
+    # start_Astar(num_of_line, size_of_line, p_location, q_location, p_foods, q_foods, pq_foods)
     print(p_foods, q_foods, pq_foods)
